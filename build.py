b2a195774b57b3a778435ab7976cc8c4aed921cc
Trung Pham <me@trungbpham.com>
2017-04-09 08:20:32 -0700

    Remove deprecated

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    visual_versions = [10, 12, 14]
     builder = ConanMultiPackager(username="pbtrung")
-    builder.add_common_builds(visual_versions=visual_versions)
+    builder.add_common_builds()
     builder.run()
